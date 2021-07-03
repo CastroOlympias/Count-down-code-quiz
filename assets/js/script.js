be0c0b9993d4ab4523286f8f947755d909532d34
Anomalous Alpha <blackcarrera@msn.com>
2021-07-02 20:29:09 -0600

    going to trim out some commented out code that isn't needed

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ var questionIdCounter = 0
 
 const quizSection = document.getElementById('quiz')
 var welcomeDiv = document.createElement('div')
-welcomeDiv.id='welcome'
+welcomeDiv.id = 'welcome'
 quizSection.appendChild(welcomeDiv)
 
 welcomeDiv = document.getElementById('welcome')
@@ -21,15 +21,15 @@ welcomeDiv = document.getElementById('welcome')
 var welcomeMsg = document.createElement('h1');
 var gameRulesMsg = document.createElement('h2');
 welcomeMsg.textContent = 'Hello World!';
-welcomeMsg.id='Hello-World';
-gameRulesMsg.id='game-rules'
+welcomeMsg.id = 'Hello-World';
+gameRulesMsg.id = 'game-rules'
 // welcomeDiv.setAttribute('style', 'margin:auto; width:50%; text-align:center;');
 welcomeDiv.appendChild(welcomeMsg);
 
 gameRulesMsg.textContent =
-  'Answer as many questions as possible while time still remains. Any wrong answer will decrease your time by 15 seconds. Good Luck!';
+    'Answer as many questions as possible while time still remains. Any wrong answer will decrease your time by 15 seconds. Good Luck!';
 //   gameRulesMsg.setAttribute('style', 'margin:auto; width:60%; text-align:center;');
-  welcome.appendChild(gameRulesMsg);
+welcome.appendChild(gameRulesMsg);
 
 var startQuiz = document.create
 
@@ -37,17 +37,18 @@ var startQuiz = document.create
 var startbtnEl = document.createElement('button');
 startbtnEl.id = 'start-quiz';
 startbtnEl.textContent = 'Start Quiz now?';
+startbtnEl.id='start-btn'
 // startbtnEl.setAttribute('style', 'font-size: 30px;','box-align: center;');
 welcomeDiv.appendChild(startbtnEl);
 
 
 // Start timer countdown
-var timeLeft = 15;
-var startQuiz = function(event) {
+var timeLeft = 90;
+var startQuiz = function (event) {
     event.preventDefault();
 
 
-    var timeInterval = setInterval(function() {
+    var timeInterval = setInterval(function () {
         timeLeft--;
         timerEl.textContent = timeLeft + ' seconds remaining';
 
@@ -61,14 +62,14 @@ var startQuiz = function(event) {
         }
 
         else {
-        timerEl.textContent = "Your time is up!";
-        clearInterval(timeInterval);
+            timerEl.textContent = "Your time is up!";
+            clearInterval(timeInterval);
         }
     }, 1000);
 
     // Start button creation for first question
     printBtnEl();
-    
+
 
 };
 startbtnEl.onclick = startQuiz;
@@ -83,61 +84,64 @@ var listBrand = [
     { q: 'What is your favorite Mel Gibson movie?', choices: ["Brave Heart", "Lethal Weapon", "Maverick", "Forever Young", "Signs"], a: 'Brave Heart' },
     { q: 'If A = 1 & B = 2 and Y = 25 & Z = 26, then what number value is Cat?.', choices: ["16", "20", "24", "28", "32"], a: '24' },
     { q: 'How much wood could a wood chuck, chuck, if a wood chuck could chuck wood?', choices: ["16 cords", "24 cords", "32 cords", "40 cords", "48 cords"], a: '48 cords' },
-    
-  ]; 
-  var index = 0;
+
+];
+var index = 0;
 //the array
 
-questionIdCounter++;
 
-var printBtnEl = function(questionIdCounter) {
-    
+questionIdCounter++;
+var printBtnEl = function (questionIdCounter) {
+  
+    const questions = document.getElementById('quiz')
+
     var currentQuestion = listBrand[index]
     var question = document.createElement("h2")
+    question.id='questions'
     question.textContent = currentQuestion.q
-    var questions = document.querySelector("#quiz")
+    
     questions.innerHTML = ""
     questions.appendChild(question)
-    
-    var questions = document.querySelector("#quiz")
-    currentQuestion.choices.forEach(function(choice,i){
-    var btn = document.createElement("button");
-    btn.setAttribute("value", choice)
-    btn.setAttribute("data-answer-id", questionIdCounter)
-    btn.onclick = checkAnswer
-    //var t = document.createElement("button");
-    btn.textContent = choice
-    //btn.appendChild(t);
-    questions.appendChild(btn);
+
+    currentQuestion.choices.forEach(function (choice, i) {
+        var btn = document.createElement("button");
+        btn.id='answers'
+        btn.setAttribute("value", choice)
+        btn.setAttribute("data-answer-id", questionIdCounter)
+        btn.onclick = checkAnswer
+        //var t = document.createElement("button");
+        btn.textContent = choice
+        //btn.appendChild(t);
+        questions.appendChild(btn);
     })
 
-    
+
     startbtnEl.remove();
     // startBtn.remove();
-    // welcomeDiv.remove();
-    // gameRulesMsg.remove();
+    welcomeDiv.remove();
+    gameRulesMsg.remove();
 }
 
-var checkAnswer = function() {
+var checkAnswer = function () {
     if (this.value === listBrand[index].a) {
         //alert("this is correct")
-        score ++;
+        score++;
         scoreEl.textContent = ' Your score is ' + score;
     } else {
         //alert("you are very wrong")
         timeLeft -= 15
     }
-    index ++;
+    index++;
     printBtnEl()
     console.log(score);
 }
 
 
 
-var endGame = function() {
+var endGame = function () {
 
 }
 
-var saveScore = function() {
+var saveScore = function () {
 
 }
